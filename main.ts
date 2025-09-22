3c454ca40197ff870c49f8ec828528eba55cf64c
Caleb-Campbell-1 <caleb.campbell@stu.ocsb.ca>
2025-09-22 19:38:31 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,24 @@
  * Created by: Caleb Campbell
  * Created on: Sep 2025
  * This program gets the current temperature in K.
-
+*/
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+basic.pause(1000)
 
-// comment
-*/
+// variables
+let temperature: number
+let temperatureKelvin: number
+
+input.onButtonPressed(Button.A, function() {
+  // input
+  // get temperature value 
+  temperature = input.temperature()
+
+  // process  
+  temperatureKelvin = temperature + 273
+
+  // outputAnswer
+  basic.clearScreen()
+  basic.showString('Temperature is: ' + temperatureKelvin.toString() + ' K. ')
+})
